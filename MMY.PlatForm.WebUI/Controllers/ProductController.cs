693225c3c9c70e41fc3a9608a2bacf57980ed77b
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-03-17 16:24:38 +0800

    product Service ok1

---
+++ MMY.PlatForm.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MMY.Services.IServices;
+
+namespace MMY.PlatForm.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProduct _product;
+        public ProductController(IProduct product)
+        {
+            _product = product;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
