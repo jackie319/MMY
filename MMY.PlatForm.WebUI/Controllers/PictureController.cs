506a372e885a3bf35641868c7b463c1c8f630fa4
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-03-21 15:57:50 +0800

    upload

---
+++ MMY.PlatForm.WebUI/Controllers/PictureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JK.Framework.Web.Model;
+
+namespace MMY.PlatForm.WebUI.Controllers
+{
+    public class PictureController : Controller
+    {
+        public ActionResult Upload()
+        {
+            return this.ResultModel(new {uid=Guid.NewGuid()});
+        }
+    }
+}
--- MMY.PlatForm.WebUI/Models/Order/OrderListViewModel.cs
+++ MMY.PlatForm.WebUI/Models/Order/OrderListViewModel.cs
@@ -8,17 +8,53 @@ namespace MMY.PlatForm.WebUI.Models.Order
     public class OrderListViewModel
     {
         public System.Guid Guid { get; set; }
+        /// <summary>
+        /// 订单号
+        /// </summary>
         public string OrderNo { get; set; }
+        /// <summary>
+        /// 订单金额
+        /// </summary>
         public int OrderAmount { get; set; }
+        /// <summary>
+        /// 产品名称
+        /// </summary>
         public string ProductName { get; set; }
+        /// <summary>
+        /// 产品数量
+        /// </summary>
         public int ProductNumber { get; set; }
+        /// <summary>
+        /// 用户
+        /// </summary>
         public string UserNickName { get; set; }
+        /// <summary>
+        ///收货地址
+        /// </summary>
         public string DeliveryAddress { get; set; }
+        /// <summary>
+        /// 支付方式
+        /// </summary>
         public string PaymentName { get; set; }
+        /// <summary>
+        /// 批次号
+        /// </summary>
         public string PayBatch { get; set; }
+        /// <summary>
+        /// 配送方式
+        /// </summary>
         public string DeliveryName { get; set; }
+        /// <summary>
+        /// 订单状态
+        /// </summary>
         public string OrderStatus { get; set; }
+        /// <summary>
+        /// 支付时间
+        /// </summary>
         public System.DateTime TimePaid { get; set; }
+        /// <summary>
+        /// 下单时间
+        /// </summary>
         public System.DateTime TimeCreated { get; set; }
 
         public static OrderListViewModel CopyFrom(Data.Model.Order order)
