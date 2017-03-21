1a95c39ba241127eba6b02706488b9a981184282
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-03-21 11:36:36 +0800

    Test Begin

---
+++ MMY.Test/OrderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MMY.Test
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
