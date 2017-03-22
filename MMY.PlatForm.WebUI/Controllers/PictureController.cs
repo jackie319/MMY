ac932fa8f1e388e6e81b51fbc2104f1b40ebada0
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-03-22 16:16:40 +0800

    upload

--- MMY.PlatForm.WebUI/Controllers/PictureController.cs
+++ MMY.PlatForm.WebUI/Controllers/PictureController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JK.Framework.Core;
+using JK.Framework.Extensions;
 using JK.Framework.Web.Model;
 
 namespace MMY.PlatForm.WebUI.Controllers
 {
     public class PictureController : Controller
     {
-        public ActionResult Upload()
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
         {
-            return this.ResultModel(new {uid=Guid.NewGuid()});
+            try
+            {
+                var url = UploadManager.SavePicture(file, "Product");
+                return this.ResultModel(url);
+            }
+            catch (CommonException ex)
+            {
+                return this.ResultError(ex.Message);
+            }
+        
         }
     }
 }
