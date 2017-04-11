5fc496e2079ef53b1eb57395608e71c38b496986
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-04-11 14:35:13 +0800

    front picture

---
+++ MMY.FrontSite.WebUI/Controllers/PictureController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JK.Framework.Core;
+using JK.Framework.Extensions;
+using JK.Framework.Web.Model;
+
+namespace MMY.FrontSite.WebUI.Controllers
+{
+    public class PictureController : Controller
+    {
+        /// <summary>
+        /// 头像上传
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            try
+            {
+                var url = UploadManager.SavePicture(file, "Product");
+                return this.ResultModel(url);
+            }
+            catch (CommonException ex)
+            {
+                return this.ResultError(ex.Message);
+            }
+
+        }
+    }
+}
