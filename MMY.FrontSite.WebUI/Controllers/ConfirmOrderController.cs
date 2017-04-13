7226f8d8dccebc16fdd9fa7b051e2a97509c9b90
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-04-13 11:44:50 +0800

    11

--- MMY.FrontSite.WebUI/Controllers/ConfirmOrderController.cs
+++ MMY.FrontSite.WebUI/Controllers/ConfirmOrderController.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using JK.Framework.Web.Filter;
+using JK.Framework.Web.Model;
 using MMY.FrontSite.WebUI.Models.ConfirmOrder;
+using MMY.Services.IServices;
 
 namespace MMY.FrontSite.WebUI.Controllers
 {
     public class ConfirmOrderController : Controller
     {
+        private IProduct _product; 
+        public ConfirmOrderController(IProduct product)
+        {
+            _product = product;
+        }
         [JKAuthorize]
         public ActionResult FromCart(IList<Guid> shoppingCartGuids )
         {
@@ -17,9 +25,19 @@ namespace MMY.FrontSite.WebUI.Controllers
         }
 
         [JKAuthorize]
-        public ActionResult FromGoods(AddConfirmOrderViewModel model)
+        [ValidationFilter]
+        public ActionResult FromBuyNow(AddConfirmOrderViewModel model)
         {
-            return null;
+            var product=_product.FindProduct(model.ProductGuid);
+            var confirm = ConfirmOrderViewModel.CopyFrom(product);
+            confirm.ProductNum = model.ProductNum;
+            confirm.OrderAmount = model.ProductNum * confirm.PromotionPrice;
+            var classification = product.ProductClassification.FirstOrDefault(q => q.Guid == model.ClassificationGuid);
+            if (classification != null)
+            {
+                confirm.ClassificationName = classification.Name;
+            }
+            return this.ResultModel(confirm);
         }
     }
 }
--- MMY.FrontSite.WebUI/Models/ConfirmOrder/AddConfirmOrderViewModel.cs
+++ MMY.FrontSite.WebUI/Models/ConfirmOrder/AddConfirmOrderViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace MMY.FrontSite.WebUI.Models.ConfirmOrder
 {
     public class AddConfirmOrderViewModel
     {
+        [Required]
         public Guid ProductGuid { get; set; }
+        [Required]
         public Guid ClassificationGuid { get; set; }
+        [Required]
         public int ProductNum { set; get; }
     }
 }
