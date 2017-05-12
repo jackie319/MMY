c0ae7a984668b2c3f1fa979284d03a3052767e63
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-05-12 15:29:59 +0800

    api

---
+++ MMY.FrontSite.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MMY.FrontSite.API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
