b86b46435ee7ee84ae918514dd21593055f2f915
jackie319 <jackie319@vip.qq.com>
2017-06-20 22:16:33 +0800

    base controller

--- MMY.FrontSite.WebApi/Controllers/UserInfoController.cs
+++ MMY.FrontSite.WebApi/Controllers/UserInfoController.cs
@@ -13,11 +13,11 @@ using System.Web.Http;
 
 namespace MMY.FrontSite.WebApi.Controllers
 {
-    public class UserInfoController : ApiController
+    public class UserInfoController : BaseApiController
     {
         private IUserAccount _UserAccount;
-        public ICacheManager _cache;
-        public UserInfoController(IUserAccount userAccount,ICacheManager cache)
+        private ICacheManager _cache;
+        public UserInfoController(IUserAccount userAccount,ICacheManager cache):base(cache)
         {
             _UserAccount = userAccount;
             _cache = cache;
@@ -30,33 +30,11 @@ namespace MMY.FrontSite.WebApi.Controllers
         [HttpGet]
         public UserInfoViewModel UserInfo()
         {
-            var sessionkey = string.Empty;
-            if (Request.Headers.Contains("sessionkey"))
-            {
-                try
-                {
-                    sessionkey = Request.Headers.GetValues("sessionkey").FirstOrDefault();
-                }
-                catch (ArgumentException)
-                {
-                }
-                if (string.IsNullOrEmpty(sessionkey))
-                {
-                    throw new MMYAuthorizeException("无效的sessionkey");
-                }
+            var userModel = GerUserModel();
+            var userInfo = _UserAccount.FindUserAccount(userModel.UserGuid);
+            var result = UserInfoViewModel.CopyFrom(userInfo);
+            return result;
 
-                var flag = _cache.IsSet(sessionkey);
-                if (!flag) throw new MMYAuthorizeException("无效的sessionkey");
-                var userModel = _cache.Get<UserModel>(sessionkey);
-                var userInfo = _UserAccount.FindUserAccount(userModel.UserGuid);
-                var result = UserInfoViewModel.CopyFrom(userInfo);
-                return result;
-            }
-            else
-            {
-                throw new CommonException("缺少参数sessionkey");
-            }
-            
         }
     }
 }
